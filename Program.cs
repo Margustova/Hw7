49ed34f6f3927bdcde29aa8a509aa377cb0a0be9
Margustova <shchennikova.ma@gmail.com>
2022-11-20 12:03:48 +0200

    task1

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
 ﻿// Семинар 7 двумерные массивы 
 // Задайте двумерный массив размером m x n, заполненный случайными целыми числами
-int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
-{
-    int[,] array = new int[rows, columns];// выделяем память под массив
+// int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+// {
+//     int[,] array = new int[rows, columns];// выделяем память под массив
 
-    for (int i=0; i< rows; i++) //сперва  проходимся по строкам
-        for (int j =0; j < columns; j++) // теперь по столбцам
-            array[i,j] = new Random().Next(minValue, maxValue + 1); // создаем массив
+//     for (int i=0; i< rows; i++) //сперва  проходимся по строкам
+//         for (int j =0; j < columns; j++) // теперь по столбцам
+//             array[i,j] = new Random().Next(minValue, maxValue + 1); // создаем массив
         
-    return array;
-    
-}
+//     return array;   
+// }
 void Show2dArray(int[,] array) //вызов двумерного массива
 {
     for(int i = 0; i < array.GetLength(0); i++) //GetLength - параметр конкретного габарита
@@ -23,14 +22,41 @@ void Show2dArray(int[,] array) //вызов двумерного массива
     Console.WriteLine(); // что бы 2 массива не сливались
 }
 
+// Console.Write("Input a number of rows: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number of columns: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a minValue: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a number of maxValue: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandom2dArray(m, n, min, max);
+// Show2dArray(myArray);
+//Задача 1 : Задайте двумерный массив размера m на n. Каждый элемент в массиве находится по формуле Aij = i+j. 
+//Выведите полученный элемент на экран.
+// метод будет генерировать новый массив, и возвращать на выходе тоже
+//на вход будет брать габариты массива
+
+int[,] CreateNew2dArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];// выделяем память под массив
+
+    for (int i=0; i< rows; i++) //сперва  проходимся по строкам
+        for (int j =0; j < columns; j++) // теперь по столбцам
+            array[i,j] = i+j;  // создаем массив , где каждый элемент создается по формуле
+        
+    return array;
+   
+}   
 Console.Write("Input a number of rows: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a minValue: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a number of maxValue: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-int[,] myArray = CreateRandom2dArray(m, n, min, max);
+int[,] myArray = CreateNew2dArray(m, n);
 Show2dArray(myArray);
+
+//Задача2: Задайте двумерный массив. найдите элементы, у которых оба индекса четные и замените эти элементы на их квадраты.
+// метод преобразующий, возвращать ничего не будем. на вход будем брать сам массив
+
+//Задача3: Задать двумерный массивб найти сумму элементов,находящихся на главной диагонали c индексами (0,0), (1,1) и тд (i==j)
